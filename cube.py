58641ce898e38169281ee0b5a50c9ede3c87b9f3
Hayden <h_n_robinson@yahoo.com>
2025-11-27 12:01:34 -0500

    cube init

--- cube.py
+++ cube.py
@@ -6,20 +6,29 @@ class Cube:
     FRONT = 4
     BACK = 5
 
-    def __init__(self):
-        self.faces = {
-            self.UP: Face(self.UP),
-            self.DOWN: Face(self.DOWN),
-            self.LEFT: Face(self.LEFT),
-            self.RIGHT: Face(self.RIGHT),
-            self.FRONT: Face(self.FRONT),
-            self.BACK: Face(self.BACK),
-        }
-
-class Face:
-    def __init__(self, direction):
-        pass
+    def __init__(self, size=3):
+        self.faces = [
+            Face(self.UP, size),
+            Face(self.DOWN, size),
+            Face(self.LEFT, size),
+            Face(self.RIGHT, size),
+            Face(self.FRONT, size),
+            Face(self.BACK, size),
+        ]
 
 class Color:
     def __init__(self, color):
-        pass
+        self.color = color
+
+class Face:
+    face_colors = {
+        Cube.UP: (255, 255, 255),
+        Cube.DOWN: (255, 255, 0),
+        Cube.LEFT: (0, 255, 0),
+        Cube.RIGHT: (0, 0, 255),
+        Cube.FRONT: (255, 0, 0),
+        Cube.BACK: (255, 100, 0),
+    }
+
+    def __init__(self, direction, size=3):
+        self.faces = [[Color(self.face_colors[direction]) for x in range(size)] for y in range(size)]
